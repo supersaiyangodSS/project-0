f17f1148369fbb692764e016f79fcc5b0ba5b4a7
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-13 21:40:00 +0530

    add display job content function

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -21,6 +21,24 @@ const JobDetails = () => {
     const [activeTab, setActiveTab] = useState(tabs[0])
 
     const onRefresh = () => {}
+
+    const displayTabContent = () => { 
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics
+                title="Qualifications"
+                points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                />
+            case "About":
+                return <JobAbout
+                info={data[0].job_description ?? "No data provided"}
+                />
+            case "Responsibilities":
+                default:
+                    break;
+        }
+     }
+
     return(
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen options={{
@@ -59,11 +77,14 @@ const JobDetails = () => {
                         companyName={data[0].employer_name}
                         location={data[0].job_country}
                         />
+
                         <JobTabs
                         tabs={tabs}
                         activeTab={activeTab}
                         setActiveTab={setActiveTab}
                         />
+
+                        {displayTabContent()}
                     </View>
                 )}
             </ScrollView>
