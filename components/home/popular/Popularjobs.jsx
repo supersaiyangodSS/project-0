17ff60106a276e288d0b9cf1dd7f79b8c6c25eba
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-14 11:48:14 +0530

    add handle card press function

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,6 +13,14 @@ const Popularjobs = () => {
     query: 'React developer',
     num_pages: 1
   });
+
+  const [selectedJob, setSelectedJob ] = useState()
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id)
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
